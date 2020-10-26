3150aa38a535644ab7306b823335ca1c1ad7ee76
Brian Habib <brianhft20@gmail.com>
2020-10-26 19:37:53 -0300

    first commit

---
+++ precache-manifest.982c6a367ceeaaab11c59e34226cf26a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bc6b6d637da78fe1c2",
+    "url": "/css/app.5c80f38f.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "180a5f4a2182a03bccfecef441f7fe10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e05438c837c278a6d91d",
+    "url": "/js/about.7500d574.js"
+  },
+  {
+    "revision": "f7bc6b6d637da78fe1c2",
+    "url": "/js/app.538c1054.js"
+  },
+  {
+    "revision": "cb82d6dee2ff633ad033",
+    "url": "/js/chunk-vendors.12f5b8e8.js"
+  },
+  {
+    "revision": "8fdf65d85533948b7d5ce5f24496cd0c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
